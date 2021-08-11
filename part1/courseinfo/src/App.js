d0e98e883c78f90623e1c5176146683452691020
Levan Tsivadze <levani.tsivadze@gmail.com>
2021-08-11 17:54:44 +0400

    name change

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,46 +1,61 @@
 import React from 'react'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  const total = exercises1 + exercises2 + exercises3;
 
-  return (
-    <div>
-      <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3} exercise1={exercises1} exercise2={exercises2} exercise3={exercises3} />
-      <Total total={total}/>
-    </div>
-  )
+  const course = {
+      name:  'Half Stack application development',
+
+      parts: [
+        {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+        {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+        {
+        name: 'State of a component',
+        exercises: 14
+      }
+  ]
+}
+    return (
+      <div>
+        <Header course={course}/>
+        <Content course={course}/>
+        <Total course={course}/>
+      </div>
+    )
 }
 
+
 const Header = (props) => {
-  return <h1>{props.course}</h1>
-   
+  return <h1>{props.course.name}</h1>
+    
 }
 
 const Content = (props) => {
+  console.log(props);
   return (
     <>
-      <Part part={props.part1} exercise={props.exercise1} />
-      <Part part={props.part2} exercise={props.exercise2} />
-      <Part part={props.part3} exercise={props.exercise3} />
+      <Part part={props.course.parts[0]} />
+      <Part part={props.course.parts[1]} />
+      <Part part={props.course.parts[2]} />
     </>
   )
 }
 
 const Total = (props) => {
   console.log(props);
-  return  <p>Number of exercises {props.total}</p>
+  const total = props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises
+
+  return <p>Number of exercises {total}</p>
 }
 
 const Part = (props) => {
-  return   <p>{props.part} {props.exercise}</p>
-   
+  console.log(props);
+
+  return <p>{props.part.name} {props.part.exercises}</p>
 }
 export default App
